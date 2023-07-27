e8005a1094adf71f06bb3290a0dcac1af5658f2d
Thiagoteixeira18 <thiagoteixeira.epo@gmail.com>
2023-07-27 15:28:19 -0300

    busar cep concluido

--- main.go
+++ main.go
@@ -1,1 +1,43 @@
-package buscarcep
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type BuscarCep struct {
+	Cep        string `json:"cep"`
+	Bairro     string `json:"bairro"`
+	Cidade     string `json:"localidade"`
+	Logradouro string `json:"logradouro"`
+	UF         string `json:"uf"`
+}
+
+func main() {
+	var cep string
+	fmt.Print("Digite o CEP: ")
+	fmt.Scanln(&cep)
+
+	url := fmt.Sprintf("https://viacep.com.br/ws/%s/json/", cep)
+	response, erro := http.Get(url)
+	if erro != nil {
+		fmt.Println("Erro ao buscar o CEP:", erro)
+		return
+	}
+	defer response.Body.Close()
+
+	var endereco BuscarCep
+	erro = json.NewDecoder(response.Body).Decode(&endereco)
+	if erro != nil {
+		fmt.Println("Erro ao decodificar a resposta:", erro)
+		return
+	}
+
+	fmt.Println("CEP:", endereco.Cep)
+	fmt.Println("Bairro:", endereco.Bairro)
+	fmt.Println("Cidade:", endereco.Cidade)
+	fmt.Println("Logradouro:", endereco.Logradouro)
+	fmt.Println("UF:", endereco.UF)
+	
+}
